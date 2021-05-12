8d172737ed78569e39e0311995c065672358eb06
Paul Biberstein <biberstein.paul@gmail.com>
2021-05-12 11:20:57 -0400

    Add types for all codap components

--- src/utils/codapPhone/types.ts
+++ src/utils/codapPhone/types.ts
@@ -1,9 +1,3 @@
-export enum CodapComponentType {
-  Graph = "graph",
-  Table = "caseTable",
-  Map = "map",
-}
-
 export enum CodapResource {
   DataContext = "dataContext",
   DataContextList = "dataContextList",
@@ -103,21 +97,91 @@ export type CodapAttribute =
   | CategoricalAttribute
   | NumericAttribute;
 
-// There are many possible component types, but they all have these fields in common
+export enum CodapComponentType {
+  Graph = "graph",
+  CaseTable = "caseTable",
+  Map = "map",
+  Slider = "slider",
+  Calculator = "calculator",
+  Text = "text",
+  WebView = "webView",
+  Guide = "guideView",
+}
+
 export interface CodapComponent {
-  type: string;
+  type: CodapComponentType;
   name: string;
   title?: string;
   dimensions: {
     width: number;
     height: number;
   };
+  position: "top" | "bottom" | { left: number; top: number };
 }
-export interface CaseTable extends CodapComponent {
-  type: "caseTable";
-  position?: string;
-  cannotClose?: boolean;
+
+export interface Graph extends CodapComponent {
+  type: CodapComponentType.Graph;
+  cannotClose: boolean;
   dataContext: string;
-  horizontalScrollOffset?: number;
-  isIndexHidden?: boolean;
+  xAttributeName: string;
+  yAttributeName: string;
+  y2AttributeName: string;
+  legendAttributeName: string;
+  enableNumberToggle: boolean;
+  numberToggleLastMode: boolean;
+}
+
+export interface CaseTable extends CodapComponent {
+  type: CodapComponentType.CaseTable;
+  cannotClose: boolean;
+  dataContext: string;
+  horizontalScrollOffset: number;
+  isIndexHidden: boolean;
+}
+
+export interface Map extends CodapComponent {
+  type: CodapComponentType.Map;
+  cannotClose: boolean;
+  dataContext: string;
+  legendAttributeName: string;
+  center: [number, number];
+  zoom: number;
+}
+
+export interface Slider extends CodapComponent {
+  type: CodapComponentType.Slider;
+  cannotClose: boolean;
+  globalValueName: string;
+  animationDirection: number;
+  animationMode: number;
+  lowerBound: number;
+  upperBound: number;
+}
+
+export interface Calculator extends CodapComponent {
+  type: CodapComponentType.Calculator;
+  cannotClose: boolean;
+}
+
+export interface Text extends CodapComponent {
+  type: CodapComponentType.Text;
+  cannotClose: boolean;
+  text: string;
+}
+
+export interface WebView extends CodapComponent {
+  type: CodapComponentType.WebView;
+  cannotClose: boolean;
+  URL: string;
+}
+
+export interface Guide extends CodapComponent {
+  type: CodapComponentType.Guide;
+  cannotClose: boolean;
+  isVisible: boolean;
+  currentItemIndex: number;
+  items: {
+    itemTitle: string;
+    url: string;
+  }[];
 }

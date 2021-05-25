4646b3ce99fee1ef821e19cf6fd4833dbbe25577
Thomas Castleman <thomascastleman0@gmail.com>
2021-05-25 12:48:25 -0400

    "select only" and "select all but" for select attributes (#38)
    * implement select-only and select-all-but modes for select attrs
    
    * format

--- src/transformation-components/SelectAttributes.tsx
+++ src/transformation-components/SelectAttributes.tsx
@@ -27,7 +27,10 @@ export function SelectAttributes({
     "",
     () => setErrMsg(null)
   );
-
+  const [mode, modeChange] = useInput<string | null, HTMLSelectElement>(
+    "selectOnly",
+    () => setErrMsg(null)
+  );
   const [lastContextName, setLastContextName] = useState<null | string>(null);
 
   /**
@@ -46,8 +49,11 @@ export function SelectAttributes({
       // extract attribute names from user's text
       const attributeNames = attributes.split("\n").map((s) => s.trim());
 
+      // select all but the given attributes?
+      const allBut = mode === "selectAllBut";
+
       try {
-        const selected = selectAttributes(dataset, attributeNames);
+        const selected = selectAttributes(dataset, attributeNames, allBut);
         await applyNewDataSet(
           selected,
           doUpdate,
@@ -59,7 +65,7 @@ export function SelectAttributes({
         setErrMsg(e.message);
       }
     },
-    [inputDataCtxt, attributes, setErrMsg, lastContextName]
+    [inputDataCtxt, attributes, mode, setErrMsg, lastContextName]
   );
 
   useContextUpdateListenerWithFlowEffect(
@@ -76,7 +82,15 @@ export function SelectAttributes({
       <p>Table to Select Attributes From</p>
       <ContextSelector onChange={inputChange} value={inputDataCtxt} />
 
-      <p>Attributes to Include in Output (1 per line)</p>
+      <p>Mode</p>
+      <select id="mode" onChange={modeChange}>
+        <option value="selectOnly">Select only the following attributes</option>
+        <option value="selectAllBut">
+          Select all but the following attributes
+        </option>
+      </select>
+
+      <p>Attributes (1 per line)</p>
       <CodapFlowTextArea onChange={attributesChange} value={attributes} />
 
       <br />
--- src/transformations/selectAttributes.ts
+++ src/transformations/selectAttributes.ts
@@ -4,16 +4,30 @@ import { reparent } from "./util";
 /**
  * Constructs a dataset with only the indicated attributes from the
  * input dataset included, and all others removed.
+ *
+ * @param dataset the dataset to transform
+ * @param attributes either the attributes to include or exclude from
+ *  the output dataset, depending on allBut
+ * @param allBut should "all but" the given attributes be selected,
+ *  or only the given attributes
  */
 export function selectAttributes(
   dataset: DataSet,
-  attributes: string[]
+  attributes: string[],
+  allBut: boolean
 ): DataSet {
+  // determine which attributes are being selected
+  const selectedAttrs = attrsToSelect(dataset, attributes, allBut);
+
+  if (selectedAttrs.length === 0) {
+    throw new Error(`output must contain at least one attribute`);
+  }
+
   // copy records, but only the selected attributes
   const records = [];
   for (const record of dataset.records) {
     const copy: Record<string, unknown> = {};
-    for (const attrName of attributes) {
+    for (const attrName of selectedAttrs) {
       // attribute does not appear on record, error
       if (record[attrName] === undefined) {
         throw new Error(`invalid attribute name: ${attrName}`);
@@ -30,7 +44,9 @@ export function selectAttributes(
 
   // filter out any attributes that aren't in the selected list
   for (const coll of allCollections) {
-    coll.attrs = coll.attrs?.filter((attr) => attributes.includes(attr.name));
+    coll.attrs = coll.attrs?.filter((attr) =>
+      selectedAttrs.includes(attr.name)
+    );
 
     // do not copy formulas: selected attributes may be separated from
     // their formula's dependencies, rendering the formula invalid.
@@ -48,4 +64,37 @@ export function selectAttributes(
     collections,
     records,
   };
+}
+
+/**
+ * Returns list of attributes that should be included in the selected
+ * output. If allBut is set, all attributes in the context that are
+ * not in the given list will be included. If it is not, the
+ * given list of attributes is returned.
+ */
+function attrsToSelect(
+  dataset: DataSet,
+  attributes: string[],
+  allBut: boolean
+): string[] {
+  // the given attributes are being selected
+  if (!allBut) {
+    return attributes;
+  }
+
+  let selected: string[] = [];
+
+  for (const coll of dataset.collections) {
+    // find all attributes within this collection that are
+    // NOT in the given attribute list
+    const attrs = coll.attrs
+      ?.map((attr) => attr.name)
+      ?.filter((name) => !attributes.includes(name));
+
+    if (attrs !== undefined) {
+      selected = selected.concat(attrs);
+    }
+  }
+
+  return selected;
 }

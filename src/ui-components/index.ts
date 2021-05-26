4298b962e62d4ed445faff73f2266675d618cfa3
Jason Chen <kbtpodifo@gmail.com>
2021-05-26 17:38:59 -0400

    Add multi select component

--- src/ui-components/index.ts
+++ src/ui-components/index.ts
@@ -4,3 +4,4 @@ export { default as CodapFlowTextArea } from "./CodapFlowTextArea";
 export { default as TransformationSubmitButtons } from "./TransformationSubmitButtons";
 export { default as ContextSelector } from "./ContextSelector";
 export { default as AttributeSelector } from "./AttributeSelector";
+export { default as MultiAttributeSelector } from "./MultiAttributeSelector";

8e73683312e2f461eb3932f0afc6bc374932114a
Thomas Castleman <thomascastleman0@gmail.com>
2021-06-25 15:35:40 -0400

    gracefully error when not embedded in CODAP

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState } from "react";
 import Transformation from "./Transformation";
 import { SavedTransformation } from "./transformation-components/types";
 import { initPhone } from "./utils/codapPhone";
+import "./App.css";
 
-// This should be a pure function (ie: only render once)
 export const App = (): ReactElement => {
-  const parsedUrl = new URL(window.location.href);
+  const [loading, setLoading] = useState<boolean>(true);
+  const loadingMsg = <p className="loading">Connecting to CODAP...</p>;
 
+  const [initError, setInitError] = useState<boolean>(false);
+  const initErrorMsg = (
+    <p className="initError">
+      Could not connect to CODAP. Please make sure you are using CODAP Flow
+      within CODAP.
+    </p>
+  );
+
+  /**
+   * Initializes a new data interactive (plugin) with the given name,
+   * and turns off loading once it has succeeded, or indicates an
+   * error should be displayed if it fails.
+   */
+  const initWithPluginName = (name: string): void => {
+    initPhone(name)
+      .then(() => {
+        setLoading(false);
+      })
+      .catch(() => {
+        setInitError(true);
+      });
+  };
+
+  const parsedUrl = new URL(window.location.href);
   const transformation = parsedUrl.searchParams.get("transform");
+  let pluginContent: JSX.Element;
 
   if (transformation === null) {
-    initPhone("CODAP Flow");
-    return <Transformation />;
+    initWithPluginName("CODAP Flow");
+    pluginContent = <Transformation />;
   } else {
     const parsedTransformation: SavedTransformation = JSON.parse(
       decodeURIComponent(transformation)
     );
 
-    initPhone(parsedTransformation.name);
+    initWithPluginName(parsedTransformation.name);
+    pluginContent = <Transformation transformation={parsedTransformation} />;
+  }
 
-    return <Transformation transformation={parsedTransformation} />;
+  if (initError) {
+    return initErrorMsg;
+  } else if (loading) {
+    return loadingMsg;
+  } else {
+    return pluginContent;
   }
 };

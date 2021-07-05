27b25e243f95124b2b38ebf265704f491d5335bd
Thomas Castleman <thomascastleman0@gmail.com>
2021-07-05 14:45:43 -0400

    initial impl of editing name/description

--- src/transformer-components/SavedTransformerView.tsx
+++ src/transformer-components/SavedTransformerView.tsx
@@ -4,6 +4,17 @@ import "./TransformerViews.css";
 import ErrorDisplay from "../ui-components/Error";
 import { SavedTransformer } from "./types";
 import { TransformerRenderer } from "./TransformerRenderer";
+import { TextArea, TextInput } from "../ui-components";
+import {
+  getInteractiveFrame,
+  notifyInteractiveFrameIsDirty,
+} from "../utils/codapPhone";
+import { useEffect } from "react";
+import { InteractiveState } from "../utils/codapPhone/types";
+import {
+  addInteractiveStateRequestListener,
+  removeInteractiveStateRequestListener,
+} from "../utils/codapPhone/listeners";
 
 /**
  * SavedTransformerView wraps a saved transformer in other important info
@@ -16,18 +27,81 @@ function SavedTransformerView({
 }): ReactElement {
   const [errMsg, setErrMsg] = useState<string | null>(null);
   const [editable, setEditable] = useState<boolean>(false);
+  const [savedTransformer, setSavedTransformer] = useState(urlTransformer);
+
+  // Load saved state from CODAP memory
+  useEffect(() => {
+    async function fetchSavedState() {
+      const savedState = (await getInteractiveFrame()).savedState;
+      if (savedState && savedState.savedTransformation) {
+        setSavedTransformer({
+          ...savedTransformer,
+          name: savedState.savedTransformation.name,
+          description: savedState.savedTransformation.description,
+        });
+      }
+    }
+    fetchSavedState();
+  }, []);
+
+  // Register a listener to generate the plugin's state
+  useEffect(() => {
+    const callback = (
+      previousInteractiveState: InteractiveState
+    ): InteractiveState => {
+      return {
+        ...previousInteractiveState,
+        savedTransformation: {
+          name: savedTransformer.name,
+          description: savedTransformer.description || "",
+        },
+      };
+    };
+
+    addInteractiveStateRequestListener(callback);
+    return () => removeInteractiveStateRequestListener(callback);
+  }, [savedTransformer]);
+
+  function notifyStateIsDirty() {
+    notifyInteractiveFrameIsDirty();
+  }
 
   return (
     <div className="transformer-view">
-      <h2>
-        {urlTransformer.name}
-        <span id="transformerBase"> ({urlTransformer.content.base})</span>
-      </h2>
-      {urlTransformer.description && <p>{urlTransformer.description}</p>}
+      {editable ? (
+        <TextInput
+          value={savedTransformer.name}
+          onChange={(e) =>
+            setSavedTransformer({ ...savedTransformer, name: e.target.value })
+          }
+          placeholder={"Transformer Name"}
+          onBlur={notifyStateIsDirty}
+        />
+      ) : (
+        <h2>
+          {savedTransformer.name}
+          <span id="transformerBase"> ({savedTransformer.content.base})</span>
+        </h2>
+      )}
+      {editable ? (
+        <TextArea
+          value={savedTransformer.description || ""}
+          onChange={(e) =>
+            setSavedTransformer({
+              ...savedTransformer,
+              description: e.target.value,
+            })
+          }
+          placeholder="Purpose Statement"
+          onBlur={notifyStateIsDirty}
+        />
+      ) : (
+        savedTransformer.description && <p>{savedTransformer.description}</p>
+      )}
       <TransformerRenderer
         setErrMsg={setErrMsg}
         errorDisplay={<ErrorDisplay message={errMsg} />}
-        transformer={urlTransformer}
+        transformer={savedTransformer}
         editable={editable}
       />
       <button

e4a450b5af1d1977974ffc73851bfc8112af700c
Thomas Castleman <thomascastleman0@gmail.com>
2021-07-23 09:46:34 -0400

    remove our language implementation

--- src/transformers/util.ts
+++ src/transformers/util.ts
@@ -1,39 +1,6 @@
 import { Collection, CodapAttribute } from "../utils/codapPhone/types";
-import { Env } from "../language/interpret";
-import { Value } from "../language/ast";
 import { Boundary, CodapLanguageType, DataSet, SingleValue } from "./types";
 import { prettyPrintCase } from "../utils/prettyPrint";
-
-/**
- * Converts a data item object into an environment for our language. Only
- * includes numeric values.
- *
- * @returns An environment from the fields of the data item.
- */
-export function dataItemToEnv(dataItem: Record<string, unknown>): Env {
-  return Object.fromEntries(
-    Object.entries(dataItem).map(([key, tableValue]) => {
-      let value;
-      // parse value from CODAP table data
-      if (
-        tableValue === "true" ||
-        tableValue === "false" ||
-        tableValue === true ||
-        tableValue === false
-      ) {
-        value = {
-          kind: "Bool",
-          content: tableValue === "true" || tableValue === true,
-        };
-      } else if (!isNaN(Number(tableValue))) {
-        value = { kind: "Num", content: Number(tableValue) };
-      } else {
-        value = { kind: "String", content: tableValue };
-      }
-      return [key, value as Value];
-    })
-  );
-}
 
 /**
  * Reparents any collections that have the given parent, to the

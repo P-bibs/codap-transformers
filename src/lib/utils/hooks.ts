37c6e904606f03f41728e89c426b20c29c867111
Thomas Castleman <thomascastleman0@gmail.com>
2021-07-26 16:26:48 -0400

    remove dead code

--- src/lib/utils/hooks.ts
+++ src/lib/utils/hooks.ts
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 import {
   getAllDataContexts,
   addNewContextListener,
@@ -64,61 +64,4 @@ export function useAttributes(context: string | null): CodapAttribute[] {
   }, [context]);
 
   return attributes;
-}
-
-interface ElementWithValue {
-  value: string;
-}
-
-export function useInput<T, E extends ElementWithValue>(
-  initialValue: T | string,
-  extraAction: (newValue: T | string) => void
-): [
-  T | string,
-  (e: React.ChangeEvent<E>) => void,
-  React.Dispatch<React.SetStateAction<T | string>>
-] {
-  const [inputValue, setInputValue] = useState<T | string>(initialValue);
-  const onChange = useCallback(
-    (event: React.ChangeEvent<E>) => {
-      setInputValue(event.target.value);
-      extraAction(event.target.value);
-    },
-    [setInputValue, extraAction]
-  );
-  return [inputValue, onChange, setInputValue];
-}
-
-/**
- * Hook that registers and unregisters a listener for updates on a
- * specific data context
- */
-export function useContextUpdateListener(
-  contextName: string,
-  callback: () => void
-): void {
-  useEffect(() => {
-    addContextUpdateListener(contextName, [], callback);
-    return () => removeContextUpdateListener(contextName, callback);
-  }, [contextName, callback]);
-}
-
-/**
- * Similar to `useContextUpdateListener`, but for callbacks (effects)
- * which rely on two strings being non-null. The implication is that
- * the effect is flowing data from the sourceContext to the
- * destinationContext, but only if both are non-null
- */
-export function useContextUpdateListenerWithFlowEffect(
-  sourceContext: string | null,
-  destinationContext: string | null,
-  flowCallback: () => void
-): void {
-  // We use an unsafe cast from string | null to string here, but it's ok because
-  // there's a check for sourceContext !== null inside the callback
-  useContextUpdateListener(sourceContext as string, () => {
-    if (sourceContext !== null && destinationContext !== null) {
-      flowCallback();
-    }
-  });
 }

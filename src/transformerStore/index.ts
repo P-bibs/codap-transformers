9eb0efe2f112045a5cfb6a5d2a304ea91d87dcc2
Thomas Castleman <thomascastleman0@gmail.com>
2021-08-02 14:08:17 -0400

    removed [fixed] label (#194)

--- src/transformerStore/index.ts
+++ src/transformerStore/index.ts
@@ -4,9 +4,6 @@ import { readableName } from "../transformers/util";
 import {
   getDataContext,
   notifyInteractiveFrameIsDirty,
-  updateDataContext,
-  getComponent,
-  updateComponent,
 } from "../lib/codapPhone";
 import {
   addInteractiveStateRequestListener,
@@ -15,7 +12,6 @@ import {
   removeContextUpdateHook,
   addContextDeletedHook,
   removeContextDeletedHook,
-  callAllContextListeners,
 } from "../lib/codapPhone/listeners";
 import { InteractiveState } from "../lib/codapPhone/types";
 import {
@@ -25,7 +21,6 @@ import {
   SafeActions,
   SafeActiveTransformationsDispatch,
   TransformationDescription,
-  TransformationOutputType,
 } from "./types";
 import {
   activeTransformationsReducer,
@@ -110,67 +105,6 @@ export function useActiveTransformations(
     async function callback(deletedContext: string) {
       const cloned = { ...activeTransformations };
       for (const input of Object.keys(cloned)) {
-        // Partition transformations based on whether or not one of their input
-        // contexts was deleted
-        const transformationsWithNewlyMissingInputs = [];
-        const restOfTransformations = [];
-        for (const description of activeTransformations[input]) {
-          if (
-            description.inputs.includes(deletedContext) ||
-            description.extraDependencies.includes(deletedContext)
-          ) {
-            transformationsWithNewlyMissingInputs.push(description);
-          } else {
-            restOfTransformations.push(description);
-          }
-        }
-
-        // Rename transformations with newly missing inputs to add a [fixed] suffix
-        for (const transformation of transformationsWithNewlyMissingInputs) {
-          if (transformation.transformer === "Partition") {
-            // If the transformer was partition we have to rename each output table
-            for (const outputContext of transformation.state.outputContexts) {
-              // Do not attempt to rename deleted contexts
-              if (outputContext === deletedContext) {
-                continue;
-              }
-              const outputContextData = await getDataContext(outputContext);
-              await updateDataContext(outputContext, {
-                title: `${outputContextData.title} [fixed]`,
-                metadata: {
-                  description: `${outputContextData.metadata?.description}\n\n An input to the transformer that created this dataset has been deleted so this dataset will no longer update.`,
-                },
-              });
-            }
-          } else if (transformation.transformer === "Editable Copy") {
-            // If the transformer was editable copy then we don't have to do anything
-          } else {
-            // Do not add [fixed] to the output if it's already been deleted
-            if (transformation.output === deletedContext) {
-              continue;
-            }
-            if (transformation.outputType === TransformationOutputType.TEXT) {
-              // If this is an SV transformer than update the output text component title
-              const outputData = await getComponent(transformation.output);
-              await updateComponent(transformation.output, {
-                title: `${outputData.title} [fixed]`,
-              });
-            } else if (
-              transformation.outputType === TransformationOutputType.CONTEXT
-            ) {
-              // If this transformer produces a dataset then rename the context
-              const outputData = await getDataContext(transformation.output);
-              await updateDataContext(transformation.output, {
-                title: `${outputData.title} [fixed]`,
-                metadata: {
-                  description: `${outputData.metadata?.description}\n\n An input to the transformer that created this dataset has been deleted so this dataset will no longer update.`,
-                },
-              });
-            }
-          }
-        }
-        callAllContextListeners();
-
         // Remove transformations with newly missing inputs
         cloned[input] = cloned[input].filter(
           (description) =>

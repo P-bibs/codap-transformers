91436a5c61ee4fbdf01f1ea3ef238f085a505ee5
Paul Biberstein <biberstein.paul@gmail.com>
2021-08-06 15:30:03 -0400

    Add better check for no active transformers

--- src/views/util.ts
+++ src/views/util.ts
@@ -5,7 +5,8 @@ export async function closePlugin(
   activeTransformations: Record<string, TransformationDescription[]>
 ): Promise<void> {
   if (
-    Object.keys(activeTransformations).length === 0 ||
+    // If there aren't any active transformers then we don't need to confirm
+    Object.values(activeTransformations).some((a) => a.length > 0) ||
     confirm(
       `Closing the transformers plugin will stop transformed datasets from updating. Are you sure you'd like to proceed?`
     )
